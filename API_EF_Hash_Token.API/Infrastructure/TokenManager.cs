4748d91aa6b5c249140b991abbcd9c6bae2030ed
Nico7522 <nico.daddabbo7100@gmail.com>
2024-01-16 12:32:05 +0100

    début implémentation token

---
+++ API_EF_Hash_Token.API/Infrastructure/TokenManager.cs
@@ -0,0 +1,10 @@
+﻿namespace API_EF_Hash_Token.API.Infrastructure
+{
+    public class TokenManager
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _secret;
+        private readonly string issuer;
+        private readonly string _audience;
+    }
+}
